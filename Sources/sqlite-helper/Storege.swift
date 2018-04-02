f8a93dde83b46bb6832b5bab77f02a53465a5d80
Evgeny Kalashnikov <lumyfly@gmail.com>
2018-04-02 20:21:02 +0300

    Updated accesses

--- Sources/sqlite-helper/Storege.swift
+++ Sources/sqlite-helper/Storege.swift
@@ -10,9 +10,9 @@ import Foundation
 import SQLite
 import apollo_mapper
 
-class Storege: MapperStorage {
+public class Storege: MapperStorage {
     
-    func save<T: Mappable>(object: Mapper, objectType: T.Type) throws {
+    public func save<T: Mappable>(object: Mapper, objectType: T.Type) throws {
         for Type in self.types {
             if objectType == Type {
                 try Type.insert(connection: self.connection, mapper: object, replace: true)
@@ -22,7 +22,7 @@ class Storege: MapperStorage {
         throw MappingError.notRegistered
     }
     
-    func save<T: Mappable>(object: T) throws {
+    public func save<T: Mappable>(object: T) throws {
         for Type in self.types where type(of: object) == Type {
             if let object = object as? Storable {
                 try Type.insert(connection: self.connection, object: object, replace: true)
@@ -35,7 +35,7 @@ class Storege: MapperStorage {
     let connection: Connection
     let types: [Storable.Type]
     
-    init(connection: Connection, types: Storable.Type...) {
+    public init(connection: Connection, types: Storable.Type...) {
         self.connection = connection
         self.types = types
     }
